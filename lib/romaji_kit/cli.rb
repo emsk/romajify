e93ef15e5e144ea2cde3535b6fb69988d9a30a9f
emsk <emsk1987@gmail.com>
2016-02-06 13:54:17 +0900

    Fix descriptions of CLI usage

--- lib/romaji_kit/cli.rb
+++ lib/romaji_kit/cli.rb
@@ -4,12 +4,12 @@ module RomajiKit
   class CLI < Thor
     class_option :upcase, type: :boolean, aliases: '-u'
 
-    desc 'hepburnize', 'Convert kana to hepburn'
+    desc 'hepburnize KANA', 'Convert kana to Hepburn romaji'
     def hepburnize(text)
       puts Converter.hepburnize(text, options[:upcase])
     end
 
-    desc 'nihon', 'Convert kana to Nihon-shiki romaji'
+    desc 'nihon KANA', 'Convert kana to Nihon-shiki romaji'
     def nihon(text)
       puts Converter.nihon(text, options[:upcase])
     end
