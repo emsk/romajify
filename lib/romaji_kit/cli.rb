4a3b9d3465ac2992125bc1d25b52301f3fcd2c49
emsk <emsk1987@gmail.com>
2016-02-06 13:42:23 +0900

    Add upcase option for CLI

--- lib/romaji_kit/cli.rb
+++ lib/romaji_kit/cli.rb
@@ -3,8 +3,9 @@ require 'thor'
 module RomajiKit
   class CLI < Thor
     desc 'hepburnize', 'Convert kana to hepburn'
+    option :upcase, type: :boolean, aliases: '-u'
     def hepburnize(text)
-      puts Converter.hepburnize(text)
+      puts Converter.hepburnize(text, options[:upcase])
     end
   end
 
