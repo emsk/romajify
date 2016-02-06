3c50a86625495082f68aa09c5580b702696c1684
emsk <emsk1987@gmail.com>
2016-02-06 13:49:39 +0900

    Add `nihon` method for CLI

--- lib/romaji_kit/cli.rb
+++ lib/romaji_kit/cli.rb
@@ -2,10 +2,16 @@ require 'thor'
 
 module RomajiKit
   class CLI < Thor
+    class_option :upcase, type: :boolean, aliases: '-u'
+
     desc 'hepburnize', 'Convert kana to hepburn'
-    option :upcase, type: :boolean, aliases: '-u'
     def hepburnize(text)
       puts Converter.hepburnize(text, options[:upcase])
+    end
+
+    desc 'nihon', 'Convert kana to Nihon-shiki romaji'
+    def nihon(text)
+      puts Converter.nihon(text, options[:upcase])
     end
   end
 
